8465fdce224ec7fefef3f366b23aa99a638af80d
ryazlee <ryan.j.lee99@gmail.com>
2018-03-23 00:26:29 -0700

    finished project! next is to make it more appealing for the user such as using the keypad for integer input

--- TaxCalculator/GroupTaxController.swift
+++ TaxCalculator/GroupTaxController.swift
@@ -9,6 +9,18 @@
 import UIKit
 
 class GroupTaxController: UIViewController {
+    var total = 0.0;
+    var tax = 0.0;
+    var totalTax = 0.0;
+    var value1 = 0.0;
+    var value2 = 0.0;
+    var value3 = 0.0;
+    var value4 = 0.0;
+    var value5 = 0.0;
+    var value6 = 0.0;
+    var value7 = 0.0;
+    var value8 = 0.0;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.addGestureRecognizer(UITapGestureRecognizer(target: self.view, action: (#selector(UIView.endEditing(_:)))))
@@ -24,15 +36,112 @@ class GroupTaxController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func taxChanged(_ sender: Any) {
+        if let value = taxAmount.text{
+            let temp = Double(value);
+            if temp != nil {
+                tax = Double(value)!/100;
+            }
+        }
     }
-    */
-
+    
+    @IBAction func calculate(_ sender: Any) {
+        
+        if let value = amount1.text{
+            let temp = Double(value);
+            if temp != nil {
+                value1 = temp!;
+            }
+        }
+        
+        if let value = amount2.text{
+            let temp = Double(value);
+            if temp != nil {
+                value2 = temp!;
+            }
+        }
+        
+        if let value = amount3.text{
+            let temp = Double(value);
+            if temp != nil {
+                value3 = temp!;
+            }
+        }
+        
+        if let value = amount4.text{
+            let temp = Double(value);
+            if temp != nil {
+                value4 = temp!;
+            }
+        }
+        
+        if let value = amount5.text{
+            let temp = Double(value);
+            if temp != nil {
+                value5 = temp!;
+            }
+        }
+        
+        if let value = amount6.text{
+            let temp = Double(value);
+            if temp != nil {
+                value6 = temp!;
+            }
+        }
+        
+        if let value = amount7.text{
+            let temp = Double(value);
+            if temp != nil {
+                value7 = temp!;
+            }
+        }
+        
+        if let value = amount8.text{
+            let temp = Double(value);
+            if temp != nil {
+                value8 = temp!;
+            }
+        }
+        
+        total = value1 + value2 + value3 + value4 + value5 + value6 + value7 + value8;
+        if tax != 0 {
+            totalLabel.text = "$" + String(total * (1+tax));
+        } else{
+            totalLabel.text = "$" + String(total);
+        }
+        totalTax = total * (tax);
+        total1.text = "$" + String(format: "%.2f", Double(value1/total) * totalTax + value1);
+        total2.text = "$" + String(format: "%.2f", Double(value2/total) * totalTax + value2);
+        total3.text = "$" + String(format: "%.2f", Double(value3/total) * totalTax + value3);
+        total4.text = "$" + String(format: "%.2f", Double(value4/total) * totalTax + value4);
+        total5.text = "$" + String(format: "%.2f", Double(value5/total) * totalTax + value5);
+        total6.text = "$" + String(format: "%.2f", Double(value6/total) * totalTax + value6);
+        total7.text = "$" + String(format: "%.2f", Double(value7/total) * totalTax + value7);
+        total8.text = "$" + String(format: "%.2f", Double(value8/total) * totalTax + value8);
+        
+        
+    }
+    
+    @IBOutlet weak var taxAmount: UITextField!
+    @IBOutlet weak var amount1: UITextField!
+    @IBOutlet weak var amount2: UITextField!
+    @IBOutlet weak var amount3: UITextField!
+    @IBOutlet weak var amount4: UITextField!
+    @IBOutlet weak var amount5: UITextField!
+    @IBOutlet weak var amount6: UITextField!
+    @IBOutlet weak var amount7: UITextField!
+    @IBOutlet weak var amount8: UITextField!
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var total1: UILabel!
+    @IBOutlet weak var total2: UILabel!
+    @IBOutlet weak var total3: UILabel!
+    @IBOutlet weak var total4: UILabel!
+    @IBOutlet weak var total5: UILabel!
+    @IBOutlet weak var total6: UILabel!
+    @IBOutlet weak var total7: UILabel!
+    @IBOutlet weak var total8: UILabel!
+    
+    
 }
