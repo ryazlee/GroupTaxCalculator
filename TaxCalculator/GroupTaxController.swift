25bda2b0d9db5bebc4d6d9830b1f17acba4a1528
ryazlee <ryan.j.lee99@gmail.com>
2018-03-24 23:06:53 -0700

    changed the icon iamge and added the tip feature based on amount.  will most likely change to percentage in the future

--- TaxCalculator/GroupTaxController.swift
+++ TaxCalculator/GroupTaxController.swift
@@ -12,6 +12,7 @@ class GroupTaxController: UIViewController {
     var total = 0.0;
     var tax = 0.0;
     var tip = 0.0;
+    var numPeople = 0.0;
     var totalTax = 0.0;
     var value1 = 0.0;
     var value2 = 0.0;
@@ -39,6 +40,7 @@ class GroupTaxController: UIViewController {
     
     
     @IBAction func taxChanged(_ sender: Any) {
+        tax = 0.0
         if let value = taxAmount.text{
             let temp = Double(value);
             if temp != nil {
@@ -48,79 +50,121 @@ class GroupTaxController: UIViewController {
     }
     
     @IBAction func calculate(_ sender: Any) {
+        var total = 0.0;
+        var numPeople = 0.0;
+        var totalTax = 0.0;
+        var value1 = 0.0;
+        var value2 = 0.0;
+        var value3 = 0.0;
+        var value4 = 0.0;
+        var value5 = 0.0;
+        var value6 = 0.0;
+        var value7 = 0.0;
+        var value8 = 0.0;
         
         if let value = amount1.text{
             let temp = Double(value);
-            if temp != nil {
+            if temp != nil && temp != 0.0 {
                 value1 = temp!;
+                numPeople += 1;
             }
         }
         
         if let value = amount2.text{
             let temp = Double(value);
-            if temp != nil {
+            if temp != nil && temp != 0.0 {
                 value2 = temp!;
+                numPeople += 1;
             }
         }
         
         if let value = amount3.text{
             let temp = Double(value);
-            if temp != nil {
+            if temp != nil && temp != 0.0 {
                 value3 = temp!;
+                numPeople += 1;
             }
         }
         
         if let value = amount4.text{
             let temp = Double(value);
-            if temp != nil {
+            if temp != nil && temp != 0.0 {
                 value4 = temp!;
+                numPeople += 1;
             }
         }
         
         if let value = amount5.text{
             let temp = Double(value);
-            if temp != nil {
+            if temp != nil && temp != 0.0 {
                 value5 = temp!;
+                numPeople += 1;
             }
         }
         
         if let value = amount6.text{
             let temp = Double(value);
-            if temp != nil {
+            if temp != nil && temp != 0.0 {
                 value6 = temp!;
+                numPeople += 1;
             }
         }
         
         if let value = amount7.text{
             let temp = Double(value);
-            if temp != nil {
+            if temp != nil && temp != 0.0 {
                 value7 = temp!;
+                numPeople += 1;
             }
         }
         
         if let value = amount8.text{
             let temp = Double(value);
-            if temp != nil {
+            if temp != nil && temp != 0.0 {
                 value8 = temp!;
+                numPeople += 1;
             }
         }
         
         total = value1 + value2 + value3 + value4 + value5 + value6 + value7 + value8;
         if tax != 0 {
-            totalLabel.text = "$" + String(total * (1+tax));
+            totalLabel.text = "$" + String(total * (1+tax) + tip);
         } else{
-            totalLabel.text = "$" + String(total);
+            totalLabel.text = "$" + String(total + tip);
         }
         totalTax = total * (tax);
-        total1.text = "$" + String(format: "%.2f", Double(value1/total) * totalTax + value1);
-        total2.text = "$" + String(format: "%.2f", Double(value2/total) * totalTax + value2);
-        total3.text = "$" + String(format: "%.2f", Double(value3/total) * totalTax + value3);
-        total4.text = "$" + String(format: "%.2f", Double(value4/total) * totalTax + value4);
-        total5.text = "$" + String(format: "%.2f", Double(value5/total) * totalTax + value5);
-        total6.text = "$" + String(format: "%.2f", Double(value6/total) * totalTax + value6);
-        total7.text = "$" + String(format: "%.2f", Double(value7/total) * totalTax + value7);
-        total8.text = "$" + String(format: "%.2f", Double(value8/total) * totalTax + value8);
         
+        if value1 != 0.0 {
+            total1.text = "$" + String(format: "%.2f", Double(value1/total) * totalTax + value1 + tip/numPeople);
+        }
+        
+        if value2 != 0.0 {
+            total2.text = "$" + String(format: "%.2f", Double(value2/total) * totalTax + value2 + tip/numPeople);
+        }
+        
+        if value3 != 0.0 {
+            total3.text = "$" + String(format: "%.2f", Double(value3/total) * totalTax + value3 + tip/numPeople);
+        }
+        
+        if value4 != 0.0 {
+            total4.text = "$" + String(format: "%.2f", Double(value4/total) * totalTax + value4 + tip/numPeople);
+        }
+        
+        if value5 != 0.0 {
+            total5.text = "$" + String(format: "%.2f", Double(value5/total) * totalTax + value5 + tip/numPeople);
+        }
+        
+        if value6 != 0.0 {
+            total6.text = "$" + String(format: "%.2f", Double(value6/total) * totalTax + value6 + tip/numPeople);
+        }
+        
+        if value7 != 0.0 {
+            total7.text = "$" + String(format: "%.2f", Double(value7/total) * totalTax + value7 + tip/numPeople);
+        }
+        
+        if value8 != 0.0 {
+            total8.text = "$" + String(format: "%.2f", Double(value8/total) * totalTax + value8 + tip/numPeople);
+        }
     }
     
     @IBAction func tipChanged(_ sender: Any) {
@@ -132,6 +176,7 @@ class GroupTaxController: UIViewController {
         }
     }
     
+    @IBOutlet weak var tipAmount: UITextField!
     @IBOutlet weak var taxAmount: UITextField!
     @IBOutlet weak var amount1: UITextField!
     @IBOutlet weak var amount2: UITextField!
@@ -141,7 +186,6 @@ class GroupTaxController: UIViewController {
     @IBOutlet weak var amount6: UITextField!
     @IBOutlet weak var amount7: UITextField!
     @IBOutlet weak var amount8: UITextField!
-    @IBOutlet weak var tipAmount: UITextField!
     
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var total1: UILabel!
